c986b09e0e2c0e1c507c9d843e8b72812062a6c0
chelseathroop <chelseathroop@gmail.com>
2015-05-13 17:12:03 -0700

    Added SD car file counts for Phu

--- SOFTWARE/PreMissionApplication.py
+++ SOFTWARE/PreMissionApplication.py
@@ -5,6 +5,12 @@ from tkFileDialog import *
 
 i = 3              #  Set up variables for rows and indexing arrays of data
 index = 0
+time = 0
+totalFileSize = 0
+SD1 = 0
+SD2 = 0
+SD3 = 0
+SD4 = 0
 dataArray = []
 arrayOfArrays = []
 
@@ -72,6 +78,43 @@ class scheduleApp() :
             # Create Array of Data Arrays
             arrayOfArrays.append([dataArray[index-8].get(),dataArray[index-7].get(),dataArray[index-6].get(),dataArray[index-5].get(),dataArray[index-4].get(),dataArray[index-3].get(),dataArray[index-2].get(), dataArray[index-1].get()])
       
+            
+            # Calculate total time and move down
+            global time
+            subHours = float(dataArray[index-3].get()) - float(dataArray[index-5].get())
+            subMins = float(dataArray[index-2].get())- float(dataArray[index-4].get())
+            if subMins <0:
+                subHours = subHours-1
+                subMins = subMins+60
+            subMins = subMins/60
+            time = round(time+subHours+subMins,2)
+            
+            # Calculate amount of files on each card
+            global totalFileSize   
+            global SD1
+            global SD2
+            global SD3
+            global SD4
+            timeSec = 3600*time 
+            totalFileSize = totalFileSize + (timeSec*float(dataArray[index-1].get())*16*1000)
+            if totalFileSize < 32000000000:
+                SD1 = SD1 +1
+            elif totalFileSize > 32000000000 and totalFileSize < 64000000000:
+                SD2 = SD2 +1
+            elif totalFileSize > 64000000000 and totalFileSize < 96000000000:
+                SD3 = SD3 +1
+            elif totalFileSize > 96000000000 and totalFileSize < 128000000000:
+                SD4 = SD4 +1
+
+
+                    
+            #Display total time amount
+            if time < 96:
+                stringTime = str(time)
+            else:
+                stringTime = 'Record Time Too Long'
+            totalTimeLabel = Label(self.root, text = 'Total Time : ').grid(row = (i+1), column = 4)
+            totatDisplay = Label (self.root, text = stringTime).grid(row = (i+1), column = 5)
             # Move buttons down
             saveParam.grid(row = (i+1), column = 7)
         
@@ -83,7 +126,8 @@ class scheduleApp() :
         # Define call back for saving paramters and writing them to the output file
         def saveParameters() :
             dataFile =  asksaveasfile(initialfile = 'dataFile.txt', initialdir = '/Users/chelseathroop/Documents/ECE/Capstone')
-      
+            
+            # Add last line of data to array of data arrays 
             arrayOfArrays.append([dataArray[index].get(),dataArray[index+1].get(),dataArray[index+2].get(),dataArray[index+3].get(),dataArray[index+4].get(),dataArray[index+5].get(),dataArray[index+6].get(),dataArray[index+7].get()])
   
             dataFile.write('{ ')    
@@ -92,17 +136,36 @@ class scheduleApp() :
                 for x in range(2):
                     dataFile.write('{')
                     for k in range(3):
+                        dataFile.write('0x')
                         dataFile.write(array[k]) 
                         dataFile.write(',')
+                    dataFile.write('0x')
                     dataFile.write(array[time])
                     dataFile.write(',')
+                    dataFile.write('0x')
                     dataFile.write(array[time+1])
                     dataFile.write(',')
+                    dataFile.write('0x')
                     dataFile.write(array[k+5]) 
                     dataFile.write('},\n')
                     time = time +2
-            dataFile.write(' }')
+            dataFile.write(' }\n')
+            dataFile.write('int const SD1=')
+            dataFile.write(str(SD1))
+            dataFile.write(';\n')
+            
+            dataFile.write('int const SD2=')
+            dataFile.write(str(SD2))
+            dataFile.write(';\n')
 
+            dataFile.write('int const SD3=')
+            dataFile.write(str(SD3))
+            dataFile.write(';\n')
+           
+            dataFile.write('int const SD4=')
+            dataFile.write(str(SD4))
+            dataFile.write(';\n')
+           
 
 #THIS IS THE MAIN LINE CREATING FIRST LINE OF ENTRY WIDGETS
 
@@ -112,19 +175,19 @@ class scheduleApp() :
         yearvcmd = (self.root.register(self.validateYear), '%P')
         yearinval = (self.root.register(self.invalidYear), '%P')
         dataArray.append(Entry(self.root, width = 5, validate = 'focusout', validatecommand = yearvcmd, invalidcommand = yearinval))
-        dataArray[0].grid(row = 2, column = 1) 
+        dataArray[index].grid(row = 2, column = 1) 
 
         # MONTH  
         monthvcmd = (self.root.register(self.validateMonth), '%P')
         monthinval = (self.root.register(self.invalidMonth), '%P')
         dataArray.append(Entry(self.root, width = 5, validate = 'focusout', validatecommand = monthvcmd, invalidcommand = monthinval))
-        dataArray[1].grid(row = 2, column = 2) 
+        dataArray[index+1].grid(row = 2, column = 2) 
  
         # DAY
         dayvcmd = (self.root.register(self.validateDay), '%P')
         dayinval = (self.root.register(self.invalidDay), '%P')
         dataArray.append(Entry(self.root, width = 5, validate = 'focusout', validatecommand = dayvcmd, invalidcommand = dayinval))
-        dataArray[2].grid(row = 2, column = 3) 
+        dataArray[index+2].grid(row = 2, column = 3) 
  
  
         # Prompt user for first start time
@@ -133,12 +196,12 @@ class scheduleApp() :
         startHourvcmd = (self.root.register(self.validateStartHour), '%P')
         startHourinval = (self.root.register(self.invalidStartHour), '%P')
         dataArray.append(Entry(self.root, width = 5, validate = 'focusout', validatecommand = startHourvcmd, invalidcommand = startHourinval))
-        dataArray[3].grid(row = 2, column = 5)
+        dataArray[index+3].grid(row = 2, column = 5)
         #MINUTE
         startMinvcmd = (self.root.register(self.validateStartMin), '%P')
         startMininval = (self.root.register(self.invalidStartMin), '%P')
         dataArray.append(Entry(self.root, width = 5, validate = 'focusout', validatecommand = startMinvcmd, invalidcommand = startMininval))
-        dataArray[4].grid(row = 2, column = 6)
+        dataArray[index+4].grid(row = 2, column = 6)
 
  
         # Prompt user for first End Time
@@ -147,14 +210,16 @@ class scheduleApp() :
         endHourvcmd = (self.root.register(self.validateEndHour), '%P')
         endHourinval = (self.root.register(self.invalidEndHour), '%P')
         dataArray.append(Entry(self.root, width = 5, validate = 'focusout', validatecommand = endHourvcmd, invalidcommand = endHourinval))
-        dataArray[5].grid(row = 3, column = 5)
+        dataArray[index+5].grid(row = 3, column = 5)
         #MIN
         endMinvcmd = (self.root.register(self.validateEndMin), '%P')
         endMininval = (self.root.register(self.invalidEndMin), '%P')
         dataArray.append(Entry(self.root, width = 5, validate = 'focusout', validatecommand = endMinvcmd, invalidcommand = endMininval))
-        dataArray[6].grid(row = 3, column = 6)
+        dataArray[index+6].grid(row = 3, column = 6)
+        
+        Label(self.root, text = 'Total Time : ').grid( row = 4, column = 4)
 
- 
+
         # Prompt user for Sample Rate
         Label(self.root, text = "Sample Rate 44, 48, 96 (kHz) : ").grid(row = 2, column = 7)
         samplevcmd = (self.root.register(self.validateSample), '%P')
